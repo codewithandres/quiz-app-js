247a66f483b53749c5e9c37032623100387ea62e
Dize dev <axiozweb2022@gmail.com>
2024-12-23 00:06:18 -0500

    se creo la funcionalidad para renderizar la pregunta y las opciones de respuesta de manera aleatoria

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,55 @@
-import './app/imporStyle';
-import { questions } from './models/questions';
+import './css/index.css';
+import './css/quiz.css';
+import './css/resul-quiz.css';
 
-console.log(questions);
+import { questions } from './models/questions';
+import type { Category, Question } from './interface';
+
+// DOM
+const $domElement = {
+    quizText: document.querySelector('.queztion-text')!,
+    answerOptions: document.querySelector('.answer-options')!,
+};
+
+let QUIZ_CATEGORY = 'mathematics';
+
+// Fetch a random question from based on the selected category
+const getRandomQuestion = (): Question => {
+    const categoryQuestions =
+        questions.find(
+            cate =>
+                cate.category.toLocaleLowerCase() ===
+                QUIZ_CATEGORY.toLowerCase()
+        )?.questions || [];
+
+    const randomQuestion =
+        categoryQuestions[
+            Math.floor(Math.random() * categoryQuestions?.length)
+        ];
+
+    return randomQuestion;
+};
+// Render the current question and its options  in the quiz
+const renderQuestion = () => {
+    const currentQuestion: Question | Category = getRandomQuestion();
+
+    const { options } = currentQuestion;
+
+    if (!currentQuestion) return;
+
+    // update UI
+    $domElement.quizText.textContent = currentQuestion.question;
+    $domElement.answerOptions.innerHTML = '';
+
+    // Create option <li> Element and append them
+    options.forEach(Option => {
+        const $li = document.createElement('li');
+
+        $li.classList.add('answer-option');
+        $li.textContent = Option;
+
+        $domElement.answerOptions.appendChild($li);
+    });
+};
+
+renderQuestion();
